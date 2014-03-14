9634ccb5f7d9cc64a0fa1a0b4346f702d4090a5f
Joel Cornett <joel.cornett@gmail.com>
2014-03-14 11:19:58 -0400

    finished build script

--- build_for_chrome.sh
+++ build_for_chrome.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+cp src/manifest.json build/manifest.json
+cp -r src/css build/css
+cp -r src/images build/images
+mkdir build/js
+cp src/js/background.js build/js/.
+cp src/js/inject-min.js build/js/.
+cp src/js/jquery-min.js build/js/.
+cp src/js/jquery-ui-1.10.4.custom.min.js build/js/.
+zip -r build build
