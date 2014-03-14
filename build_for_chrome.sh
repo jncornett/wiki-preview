3899a2124c4821033b6048b3f6530a45876b5207
Joel Cornett <joel.cornett@gmail.com>
2014-03-14 12:50:14 -0400

    fixed build script

--- build_for_chrome.sh
+++ build_for_chrome.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+mkdir build
 cp src/manifest.json build/manifest.json
-cp -r src/css build/css
-cp -r src/images build/images
+cp -r src/css build/
+cp -r src/images build/
 mkdir build/js
 cp src/js/background.js build/js/.
 cp src/js/inject-min.js build/js/.
